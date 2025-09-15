e945bd7663b5e343769cc335d93ddd78ba47e8fe
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2025-09-15 19:19:30 +0300

    feat: Add ZRA Segment DocType and related files

---
+++ onsitenergy/onsitenergy/doctype/zra_segment/zra_segment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Douglas Nkubitu and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ZRA_Segment(Document):
+	pass
